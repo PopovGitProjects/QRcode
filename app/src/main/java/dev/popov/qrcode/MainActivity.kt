86a86869f0777e76d0f00dbb977497fab90fbfbf
Popov <popov.workmail@gmail.com>
2021-11-16 22:01:50 +0500

    Добавил кнопку выхода из приложения.

--- app/src/main/java/dev/popov/qrcode/MainActivity.kt
+++ app/src/main/java/dev/popov/qrcode/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.popov.qrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.popov.qrcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonBack?.setOnClickListener{
+            this.finish()
+        }
     }
 }

50cd934ad4e6ad23088b3e21c398b59a27bb0a7a
Popov <popov.workmail@gmail.com>
2021-11-18 21:29:15 +0500

    изменил кнопку выхода из приложения заблокировал преход в темный режим. Не помогло, на API 30 все равно система переходит в темный режим!

--- app/src/main/java/dev/popov/qrcode/MainActivity.kt
+++ app/src/main/java/dev/popov/qrcode/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.popov.qrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dev.popov.qrcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
         binding.buttonClose?.setOnClickListener{
             finish()

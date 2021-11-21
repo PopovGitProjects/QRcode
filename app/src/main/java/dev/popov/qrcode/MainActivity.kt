874a9ab603f7ba56bf06c0138ded8cd04c7a7f67
Popov <popov.workmail@gmail.com>
2021-11-21 21:30:45 +0500

    Изменил метод вывода изображения на экран со статичной картинки на генерацию изображения. Необходимо к генератору QR кодов нужно добавить и сканер!

--- app/src/main/java/dev/popov/qrcode/MainActivity.kt
+++ app/src/main/java/dev/popov/qrcode/MainActivity.kt
@@ -1,8 +1,13 @@
 package dev.popov.qrcode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.zxing.WriterException
+import dev.popov.qrcode.constants.Constants
 import dev.popov.qrcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +17,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
+        generateQR(Constants.URL_QR_CODE)
         binding.buttonClose?.setOnClickListener{
             finish()
+        }
+        binding.generate?.setOnClickListener {
+            generateQR("Done is real work!!!")
+        }
+    }
+    private fun generateQR(url: String){
+        val qrGenerator = QRGEncoder(url, null, QRGContents.Type.TEXT, 900)
+        try {
+            val bMap = qrGenerator.bitmap
+            binding.qrCode?.setImageBitmap(bMap)
+        }catch (e: WriterException){
+            Log.v("TAG", e.toString())
         }
     }
 }
---
+++ app/src/main/java/dev/popov/qrcode/constants/Constants.kt
@@ -0,0 +1,6 @@
+package dev.popov.qrcode.constants
+
+object Constants {
+    const val URL_QR_CODE
+    = "https://www.gosuslugi.ru/vaccine/cert/verify/c2104d48-79c2-4755-bdad-9e1ec0e87145"
+}
